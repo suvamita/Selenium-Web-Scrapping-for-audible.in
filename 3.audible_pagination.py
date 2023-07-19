a9d8178bc7666c438627a28ba93603a17a5f41e2
Suvamita Sadhukhan <103322378+suvamita@users.noreply.github.com>
2023-07-20 00:32:24 +0530

    audible_pagination.py

--- 3.audible_pagination.py
+++ 3.audible_pagination.py
@@ -2,47 +2,54 @@ from selenium import webdriver
 import pandas as pd
 import time
 from selenium.webdriver.chrome.service import Service
-
-web = "https://www.audible.com/adblbestsellers?ref=a_search_t1_navTop_pl0cg1c0r0&pf_rd_p=adc4b13b-d074-4e1c-ac46-9f54aa53072b&pf_rd_r=1F7DV0MPHV77Z61RX566"
-path = 'C:/Users/user/Downloads/geckodriver-v0.33.0-win-aarch64/geckodriver.exe'
-driver = webdriver.Chrome(service=Service(chrome_driver_path))
-
+from selenium import *
+web ='https://www.audible.in/adblbestsellers?ref=a_hp_t1_navTop_pl1cg0c1r0&pf_rd_p=4e150d5e-ca98-47fb-823b-f6fcb252aced&pf_rd_r=5ZNPZJB39ET4E6GHZZ1S&pageLoadId=4l3aH0gjemHWJBG0&creativeId=2e6787a2-0cd0-4a6e-afe0-05766cd505e5'
+path = 'C:/Users/user/Downloads/chromedriver.exe'
+service = Service(executable_path=path)
+driver = webdriver.Chrome(service=service)
 driver.get(web)
 driver.maximize_window()
 
 # Pagination 1
-pagination = driver.find_element_by_xpath('//ul[contains(@class, "pagingElements")]')  # locating pagination bar
-pages = pagination.find_elements_by_tag_name('li')  # locating each page displayed in the pagination bar
+pagination = driver.find_element(by='xpath',value='//ul[contains(@class, "pagingElements")]')  # locating pagination bar
+pages = pagination.find_elements(by='tag name',value='li')  # locating each page displayed in the pagination bar
 last_page = int(pages[-2].text)  # getting the last page with negative indexing (starts from where the array ends)
 
 book_title = []
-book_author = []
+book_writer = []
 book_length = []
-
+book_narrator=[]
+book_releasedate=[]
+book_language=[]
+book_rating=[]
 # Pagination 2
 current_page = 1   # this is the page the bot starts scraping
 
 # The while loop below will work until the the bot reaches the last page of the website, then it will break
 while current_page <= last_page:
     time.sleep(2)  # let the page render correctly
-    container = driver.find_element_by_class_name('adbl-impression-container ')
-    products = container.find_elements_by_xpath('.//li[contains(@class, "productListItem")]')
-    # products = container.find_elements_by_xpath('./li')
+    container = driver.find_element(by='class name', value='adbl-impression-container ')
+    products = container.find_elements(by='xpath', value='.//li[contains(@class, "productListItem")]')
 
-    for product in products:
-        book_title.append(product.find_element_by_xpath('.//h3[contains(@class, "bc-heading")]').text)
-        book_author.append(product.find_element_by_xpath('.//li[contains(@class, "authorLabel")]').text)
-        book_length.append(product.find_element_by_xpath('.//li[contains(@class, "runtimeLabel")]').text)
+    for product in products:       
+        book_title.append(product.find_element(by='xpath', value='.//h3[contains(@class, "bc-heading")]').text)
+        book_writer.append(product.find_element(by='xpath', value='.//li[contains(@class, "authorLabel")]').text)
+        book_length.append(product.find_element(by='xpath', value='.//li[contains(@class, "runtimeLabel")]').text)
+        book_narrator.append(product.find_element(by='xpath', value='..//li[contains(@class, "narratorLabel")]').text)
+        book_releasedate.append(product.find_element(by='xpath', value='.//li[contains(@class, "releaseDateLabel")]').text)
+        book_rating.append(product.find_element(by='xpath', value='.//li[contains(@class, "ratingsLabel")]').text)
+        book_language.append(product.find_element(by='xpath', value='.//li[contains(@class, "languageLabel")]').text)
+        
 
     current_page = current_page + 1  # increment the current_page by 1 after the data is extracted
     # Locating the next_page button and clicking on it. If the element isn't on the website, pass to the next iteration
     try:
-        next_page = driver.find_element_by_xpath('.//span[contains(@class , "nextButton")]')
+        next_page = driver.find_element(by='xpath',value='.//span[contains(@class , "nextButton")]')
         next_page.click()
     except:
         pass
 
 driver.quit()
 
-df_books = pd.DataFrame({'title': book_title, 'author': book_author, 'length': book_length})
-df_books.to_csv('books_pagination.csv', index=False)
+df_books = pd.DataFrame({'Title': book_title, 'Writer': book_writer, 'Narrator': book_narrator, 'Length':book_length , 'Release Date': book_releasedate, 'Language': book_language , 'Ratings':book_rating})
+df_books.to_csv('Audible_books_pagination.csv', index=False)
