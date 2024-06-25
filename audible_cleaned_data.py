0e438b0c50e273b1fd1fcb4bc6785fef64ef0529
Suvamita Sadhukhan <103322378+suvamita@users.noreply.github.com>
2024-06-25 20:45:54 +0530

    Update audible_cleaned_data.py

--- audible_cleaned_data.py
+++ audible_cleaned_data.py
@@ -3,16 +3,16 @@ import re
 
 # Read data from CSV file
 df = pd.read_csv('Audible_books_pagination.csv')
+
 # Data cleaning steps
 # Remove unnecessary prefixes
 df['Writer'] = df['Writer'].str.replace('Written by: ', '')
 df['Length'] = df['Length'].str.replace('Length: ', '')
-df['Title'] = df['Title'].str.replace(r'\d+\.\s+', '')
+df['Title'] = df['Title'].str.replace(r'\d+\.\s+', '', regex=True)  # Explicitly set regex=True
 df['Narrator'] = df['Narrator'].str.replace('Narrated by: ', '')
 df['Release Date'] = df['Release Date'].str.replace('Release Date: ', '')
 df['Language'] = df['Language'].str.replace('Language: ', '')
 df['Ratings'] = df['Ratings'].str.replace(' ratings ', '')
-
 
 # Handle special characters using str.normalize
 df['Writer'] = df['Writer'].str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
@@ -25,7 +25,10 @@ def extract_length_minutes(length_str):
     return hours * 60 + minutes
 
 df['Length'] = df['Length'].apply(extract_length_minutes)
+
+# Adjust title to remove numeric prefixes
 df['Title'] = df['Title'].apply(lambda x: x.split('. ', 1)[-1])
+
 # Extract numeric ratings
 def extract_numeric_rating(rating_str):
     try:
@@ -34,7 +37,6 @@ def extract_numeric_rating(rating_str):
         return None
 
 df['Ratings'] = df['Ratings'].apply(extract_numeric_rating)
-# print(df)
 
 # Save cleaned dataset to a new CSV file
 df.to_csv('Audible_books_Cleaned_Data.csv', index=False)
